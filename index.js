76cb50f9c228c51ca336f0fcf2b327dd7d22e177
tehreem-fatima-quecko <tehreem.quecko@gmail.com>
2026-06-02 15:25:05 +0500

    updated

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var app = express();
 var cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +18,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.json({ error: "No file uploaded" });
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
